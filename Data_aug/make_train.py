0e7521d563d5572a893b6438661cf0256411ae90
liyihan093730 <liyihan093730@gmail.com>
2017-12-13 00:06:35 -0500

    Add files via upload

---
+++ Data_aug/make_train.py
@@ -0,0 +1,20 @@
+import os
+#0:224,1:464,2:251,3:454,4:260,5:282
+cwd = os.getcwd()
+"""
+with open("train.txt", "w") as outF:
+    for j in range(7):
+        k = os.path.join(cwd,'aug/','%d'%j)
+        ls = os.listdir(k)
+        for img in ls:
+            print('%s/%s %d'%(k,img,j))
+            outF.writelines('%s/%s %d\n'%(k,img,j))
+"""
+with open("test.txt", "w") as outF:
+    for j in range(6):
+        k = os.path.join(cwd,'test/','%d'%j)
+        ls = os.listdir(k)
+        for img in ls:
+            if img[-3:] == 'jpg':
+                print('%s/%s'%(k,img))
+                outF.writelines('%s/%s %d\n'%(k,img,j))
